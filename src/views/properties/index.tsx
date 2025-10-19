539aeaa32cf3cc25b0603271f807759c120c2db6
David Udoh <udohdavid004@gmail.com>
2025-10-19 13:36:25 +0100

    fix: fixed grid cols

--- src/views/properties/index.tsx
+++ src/views/properties/index.tsx
@@ -5,7 +5,7 @@ const Properties = () => {
   const lands: Land[] = landsData;
   return (
     <div className="flex w-full flex-col gap-5">
-      <div className="w-ful mt-3 flex h-fit flex-col gap-5 lg:grid lg:grid-cols-12">
+      <div className="w-ful mt-3 flex h-fit flex-col gap-5 md:grid md:grid-cols-12">
         {lands.map((land) => (
           <div className="col-span-6 lg:!mb-0">
             <LandCard key={land.id} land={land} />
